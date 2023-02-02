5edb6219e2ab49fbd5aac4b492337ab3593ad97e
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-02 13:44:41 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@predictTheOutput/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+  int a=4, b=6, c=8;
+  float d=10;
+  //printf("The answer is %f", a+b/c+d);
+  //printf("The answer is %d", b%c+a*a);
+  //printf("The answer is %f", b%a+c*d);
+  //printf("The answer is %d", (d%a));
+  //printf("The answer is %d", a/(c%a));
+  //printf("The answer is %f", a*(d+c)*b);
+  //printf("The answer is %d", a-a*a-b);
+  //printf("The answer is %d", c*(c+b)/a);
+  //printf("The answer is %f", d/a*b);
+  //printf("The answer is %f", c-b-a*d);
+  //printf("The answer is %f", c+b-d*a);
+  //printf("The answer is %d", (b-c)*(a+b));
+  //printf("The answer is %d", b%c*2);
+  //printf("The answer is %d", a/0+6*a);
+  //printf("The answer is %d", c%b%a);
+  //printf("The answer is %f", d%a+a+c);
+  //printf("The answer is %d", b*c%a);
+  //printf("The answer is %f", d-c*a);
+  //printf("The answer is %d", b*a%a+c);
+  printf("The answer is %d", 0/a+0/b*3);
+  return 0;
+}
